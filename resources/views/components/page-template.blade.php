ff242b92109df64d2016f1bf0b551f1123727db4
Adrian <adrianluizspark@gmail.com>
2023-05-04 23:40:00 -0300

    fix favicon

--- resources/views/components/page-template.blade.php
+++ resources/views/components/page-template.blade.php
@@ -6,9 +6,9 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.png">
+  <link rel="icon" type="image/png" href="https://vanciscapital.com/wp-content/uploads/2022/11/cropped-Vancis-Capital-New-Logo-2022-White-Gold-Recovered.png">
   <title>
-    Material Dashboard PRO Laravel by Creative Tim
+    Vancis Offices
   </title>
 
   <!--     Metas    -->
