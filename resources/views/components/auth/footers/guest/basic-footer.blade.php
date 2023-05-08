0b792d29b5940e90edfb102c68b3917efb15df2e
Adrian <adrianluizspark@gmail.com>
2023-05-08 03:12:36 -0300

    Insert DataTable newss

--- resources/views/components/auth/footers/guest/basic-footer.blade.php
+++ resources/views/components/auth/footers/guest/basic-footer.blade.php
@@ -1,37 +1,65 @@
-<!-- @props(['textColor'])
+@props(['textColor'])
+<footer class="footer position-absolute bottom-2 py-2 w-100">
+<style>
+.col img {
+  max-width: 5% !important;
+}  
+.col a {
+  color: white !important;
+  font-size: 13px !important;
+}
+</style>
+<div class="container">
+  <div class="row">
+    <div class="col">
+    <img src="https://vanciscapital.com/wp-content/uploads/2021/09/Flag-Vanuatu.jpeg" class="flag">
+          <a href="">
+          BLD Ludovic Degaillande Poteau 325/308 Anabrou 1,<br>
+          Port Vila, Vanuatu
+          </a>
+    </div>
+    <div class="col">
+    <img src="https://vanciscapital.com/wp-content/uploads/2022/03/UAE.jpeg" class="flag">
+          <a href="">
+              Office No: 1911 B2B Office Tower Al Abraj Street, Business Bay - Dubai UAE
+          </a>
+    </div>
+    <div class="col">
+    <img src="https://vanciscapital.com/wp-content/uploads/2021/09/Turkey-e1632660970933.jpeg" class="flag">
+      <a href="">
+              T3 Blok Office 13. 2nd floor,  Kagithane, Istanbul Turkey
+          </a>
+    </div>
+  </div>
+</div>
+</footer>
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <footer class="footer position-absolute bottom-2 py-2 w-100">
     <div class="container">
-      <div class="row align-items-center justify-content-lg-between">
-        <div class="col-12 col-md-6 my-auto">
-          <div class="{{ $textColor}} copyright text-center text-sm text-lg-start">
-            © <script>
-              document.write(new Date().getFullYear())
-            </script>,
-            made with <i class="fa fa-heart" aria-hidden="true"></i> by
-            <a href="https://www.creative-tim.com" class="{{ $textColor}} font-weight-bold" target="_blank">Creative Tim</a> & <a href="https://www.updivision.com" class="{{ $textColor}} font-weight-bold" target="_blank">UPDIVISION</a>
-            for a better web.
-          </div>
+      <div class="row">     
+
+        <div class="col-md-4" style="width: 33% !important">
+          <img src="https://vanciscapital.com/wp-content/uploads/2021/09/Flag-Vanuatu.jpeg" class="flag">
+          <a href="" style="color: white">
+          BLD Ludovic Degaillande Poteau 325/308 Anabrou 1,
+          Port Vila, Vanuatu
+          </a>
         </div>
-        <div class="col-12 col-md-6">
-          <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com" class="nav-link {{ $textColor}}" target="_blank">Creative Tim</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.updivision.com" class="nav-link {{ $textColor}}" target="_blank">UPDIVISION</a>
-            </li>
-            
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com/presentation" class="nav-link {{ $textColor}}" target="_blank">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com/blog" class="nav-link {{ $textColor}}" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com/license" class="nav-link pe-0 {{ $textColor}}" target="_blank">License</a>
-            </li>
-          </ul>
+
+        <div class="col-md-4" style="width: 33% !important">
+          <img src="https://vanciscapital.com/wp-content/uploads/2022/03/UAE.jpeg" class="flag">
+          <a href="" style="color: white">
+              Office No: 1911 B2B Office Tower Al Abraj Street, Business Bay - Dubai UAE
+          </a>
         </div>
+
+        <div class="col-md-4" style="width: 33% !important">
+          <img src="https://vanciscapital.com/wp-content/uploads/2021/09/Turkey-e1632660970933.jpeg" class="flag">
+          <a href="">
+              T3 Blok Office 13. 2nd floor,  Kagithane, Istanbul Turkey
+          </a>
+        </div>
+
       </div>
     </div>
   </footer> -->
