e2fbb488eb50672fe95c2b1d1b9730795e75f349
Adrian <adrianluizspark@gmail.com>
2023-05-11 08:32:31 -0300

    change currencysss

--- resources/views/components/auth/navbars/sidebar.blade.php
+++ resources/views/components/auth/navbars/sidebar.blade.php
@@ -94,9 +94,22 @@
                         <span class="nav-link-text ms-2 ps-1">Receivables</span>
                     </a>
                 </li>    
-                @endcan               
-                
+                @endcan
+
                 @can('viewAny', App\Models\User::class)
+                <li class="nav-item">
+                <a href="{{ route('user-profile') }}"
+                        class="nav-link text-white {{ $activePage == 'user-profile' ? ' active ' : '' }} "
+                        aria-controls="dubai" role="button" aria-expanded="false" onMouseOver="this.style.color='#0F0'"
+                        style="margin-left: 12px">
+                        <i class="material-icons-round opacity-10">settings</i>
+                        <!-- <img src="https://flagcdn.com/20x15/ae.png" alt=""> -->
+                        <span class="nav-link-text ms-2 ps-1">Settings</span>
+                    </a>                                            
+                </li>
+                @endcan
+                
+                @can('viewAny', App\Models\Dubai::class)
                 <li class="nav-item">
                 <a href="{{ route('users') }}"
                         class="nav-link text-white {{ $activePage == 'laravel-examples' ? ' active ' : '' }} "
